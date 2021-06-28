0a4e06424c600b438574778180ede19db3d907e5
shashkov-m <43266908+shashkov-m@users.noreply.github.com>
2021-06-29 01:13:34 +0300

    0

--- HomeWorkApp/Friends/FriendsTableViewController.swift
+++ HomeWorkApp/Friends/FriendsTableViewController.swift
@@ -1,96 +1,90 @@
-//
-//  SearchViewController.swift
-//  HomeWorkApp
-//
-//  Created by Шашков Максим Алексеевич on 02.03.2021.
-//
+////
+////  SearchViewController.swift
+////  HomeWorkApp
+////
+////  Created by Шашков Максим Алексеевич on 02.03.2021.
+////
 import UIKit
 import RealmSwift
 import SDWebImage
 import FirebaseFirestore
 class FriendsTableViewController: UITableViewController {
+    
+    private let friendsRequest = FriendsRequest()
+    private let albumRequest = AlbumRequest()
+    private var user_id:Int = 0
+    private let realm = try! Realm ()
+    private var friends:Results<FriendsRealmEntity>?
+    private var token :NotificationToken?
+    private let firestore = Firestore.firestore()
+    private let date = Date ()
+    private let df = DateFormatter ()
 
-    let friendsRequest = FriendsRequest()
-    let albumRequest = AlbumRequest()
-    var user_id:Int = 0
-    let realm = try! Realm ()
-    var friends:Results<FriendsRealmEntity>?
-    var token :NotificationToken?
-    let firestore = Firestore.firestore()
-    let date = Date ()
-    let df = DateFormatter ()
-    
-    @IBAction func reloadButton(_ sender: Any) {
-        friendsRequest.getFriendsList()
-        tableView.reloadData()
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         friendsRequest.getFriendsList()
         friends = realm.objects(FriendsRealmEntity.self)
-        print("Realm path = \(String(describing: realm.configuration.fileURL))")
-        token = friends?.observe {[weak self] changes in
+        tableView.register(UINib(nibName: "FriendsTableViewCell", bundle: nil), forCellReuseIdentifier: "FriendsCell")
+        
+        token = friends?.observe { [weak self] changes in
             switch changes {
             case .initial:
                 self?.tableView.reloadData()
             case .update(_, deletions: _, insertions: _, modifications: _):
+                print ("update data")
                 self?.tableView.reloadData()
             case .error(let error):
                 print (error.localizedDescription)
             }
         }
+
+        print("Realm path = \(String(describing: realm.configuration.fileURL))")
         df.dateFormat = "dd-MM-yyyy HH:mm:ss"
         let dateString = df.string(from: date)
+
         firestore.collection("users").document(Session.session.userId).setData(["last auth":"\(dateString)"]) { error in
             if let error = error {
                 print ("firestore error: \(error)")
             } else {
-                print ("firestore success")
+                print ("firestore data write success")
             }
         }
-        
+
     }
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return friends?.count ?? 0
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FriendsCell", for: indexPath)
-        guard let friends = friends else {return cell}
-        let user = friends [indexPath.row] //sortedUserDictionary[indexPath.section].value [indexPath.row]
-        //        let paragraphStyle = NSMutableParagraphStyle()
-        //       paragraphStyle.firstLineHeadIndent = 55
-        //       let attributes = [NSAttributedString.Key.paragraphStyle: paragraphStyle]
-        cell.textLabel?.numberOfLines = user.city != "" ? 2 : 1
-        let text = user.city != "" ? "\(user.name)\n\(user.city!)" : "\(user.name)"
-        cell.textLabel?.text = text
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FriendsCell", for: indexPath) as! FriendsTableViewCell
+        guard let friends = friends else { return cell }
+        let user = friends [indexPath.row]
+        cell.nameLabel.text = "\(user.name)"
+        if let city = user.city {
+            cell.cityLabel.text = "\(city)"
+        }
         let url = URL(string: user.photo)
-        cell.imageView?.sd_setImage(with: url, placeholderImage: #imageLiteral(resourceName: "photo_2021-05-25 21.31.48"))
-        cell.imageView?.layer.cornerRadius = 25
-        cell.imageView?.layer.masksToBounds = true
-        //       cell.textLabel?.attributedText = NSAttributedString (string: text, attributes: attributes)
+        cell.avatarView.sd_setImage (with: url, placeholderImage: nil)
         return cell
     }
-    
-    
+
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let user = friends![indexPath.row]
+        guard let friends = friends else { return }
+        let user = friends [indexPath.row]
         user_id = user.id
         performSegue(withIdentifier: "toAlbums", sender: self)
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)        
+        super.viewDidAppear(animated)
         tableView.reloadData()
-    }
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50.0
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -99,8 +93,9 @@ class FriendsTableViewController: UITableViewController {
             albumsVC.user_id = user_id
         }
     }
-    
+
     deinit {
         token?.invalidate()
     }
 }
+
