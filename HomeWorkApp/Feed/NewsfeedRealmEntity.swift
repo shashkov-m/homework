4f39a921c953085695d030d4748a61f90bdf56d8
shashkov-m <43266908+shashkov-m@users.noreply.github.com>
2021-07-04 01:39:36 +0300

    infinityScrolling

--- HomeWorkApp/Feed/NewsfeedRealmEntity.swift
+++ HomeWorkApp/Feed/NewsfeedRealmEntity.swift
@@ -10,6 +10,19 @@ class NewsfeedRealmEntuty:Object {
     @objc dynamic var reposts:Int = 0
     @objc dynamic var user_likes:Int = 0
     var attachments = List <NewsfeedRealmAttachment> ()
+    
+    convenience init (item: NewsfeedRequest.Items) {
+        self.init ()
+        self.id = abs(item.source_id ?? 0)
+        self.date = item.date ?? 0
+        self.text = item.text
+        self.marked_as_ads = item.marked_as_ads ?? 0
+        self.likes = item.likes?.count ?? 0
+        self.user_likes = item.likes?.user_likes ?? 0
+        self.comments = item.comments?.count ?? 0
+        self.reposts = item.reposts?.count ?? 0
+        self.views = item.views?.count ?? 0
+    }
 }
 
 class NewsfeedRealmAttachment:Object {
@@ -22,6 +35,20 @@ class NewsfeedRealmOwner:Object {
     @objc dynamic var id:Int = 0
     @objc dynamic var name:String = ""
     @objc dynamic var photo:String = ""
+    
+    convenience init (item: NewsfeedRequest.Groups) {
+        self.init()
+        self.id = item.id
+        self.name = item.name
+        self.photo = item.photo_50
+    }
+    
+    convenience init (item: NewsfeedRequest.Profiles) {
+        self.init()
+        self.id = item.id
+        self.name = "\(item.first_name) \(item.last_name ?? "")"
+        self.photo = item.photo_50
+    }
     
     override class func primaryKey() -> String? {
         return "id"
